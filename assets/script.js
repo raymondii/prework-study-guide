0d01a1640bf269c5419ebf8a172f098c4b829410
Ray Dixon <ray@Rays-MacBook-Air.local>
2023-11-30 18:36:26 -0500

    added JavaScript to the prework application

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+var topics = ["HTML", "CSS", "GIT", "JavaScript"];
+for(var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+}
+
+/*var topic = "HTML";
+
+
+if (topic === "HTML") {
+    console.log("let's study HTML!");
+} else if (topic === "CSS") {
+    console.log("let's study CSS!");
+} else if (topic === "Git") {
+    console.log("let's study Git!");
+} else if (topic === "JavaScript") {
+    console.log("let's study JavaScript!");
+} else {
+    console.log("Error 1");
+}
+
+*/
+
+//var shapes = ["triangle", "square", "pentagon", "circle"];
+//console.log(shape[0]);
+
+//for(var x = 0; x < shapes.length; x++) {
+   // console.log(shapes[x]);
+//}
