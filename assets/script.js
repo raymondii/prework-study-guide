e7ed2d59425fd7d23f5f09633046a67ed2e9dec9
Ray Dixon <ray@Rays-MacBook-Air.local>
2023-12-01 00:37:54 -0500

    added JS functions to the prework application

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,35 @@
 var topics = ["HTML", "CSS", "GIT", "JavaScript"];
-for(var x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+//var topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+
+console.log("Here are the topics we learned through Prework:");
+function listTopics() {
+    for(var x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
+    
+}
+listTopics();
+
+//var topic = "HTML";
+
+function selectTopic() {
+    if (randomTopic === "HTML") {
+        console.log("let's study HTML!");
+    } else if (randomTopic === "CSS") {
+        console.log("let's study CSS!");
+    } else if (randomTopic === "Git") {
+        console.log("let's study Git!");
+    } else if (randomTopic === "JavaScript") {
+        console.log("let's study JavaScript!");
+    } else {
+        console.log("Please Try Again. (MacOS - command + R)");
+    }
 }
 
-/*var topic = "HTML";
-
-
-if (topic === "HTML") {
-    console.log("let's study HTML!");
-} else if (topic === "CSS") {
-    console.log("let's study CSS!");
-} else if (topic === "Git") {
-    console.log("let's study Git!");
-} else if (topic === "JavaScript") {
-    console.log("let's study JavaScript!");
-} else {
-    console.log("Error 1");
-}
-
-*/
+console.log("Which topic should we study first?")
+selectTopic()
 
 //var shapes = ["triangle", "square", "pentagon", "circle"];
 //console.log(shape[0]);
